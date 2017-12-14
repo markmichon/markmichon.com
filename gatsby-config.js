4b2fcbb112c5a7b63af48ea7ff1649982e9a66c2
markmichon <hello@markmichon.com>
2017-12-14 12:25:45 -0500

    Remove preact until development levels out.

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-catch-links",
     "gatsby-plugin-styled-components",
-    "gatsby-plugin-preact",
+    // "gatsby-plugin-preact",
 
     {
       resolve: "gatsby-source-filesystem",
