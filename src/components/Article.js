563e74449763f695228ff35165e61d049b4b7d12
markmichon <hello@markmichon.com>
2017-12-14 12:26:27 -0500

    Update article padding
    Padding consistency change to be more in line with other pages.

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,7 +3,7 @@ import styled from "styled-components"
 
 const Article = styled.article`
   color: ${p => p.theme.copy};
-  padding: ${p => p.theme.halfUnit};
+  padding: ${p => p.theme.baseUnit};
   max-width: ${p => p.theme.measure};
   width: 100%;
   margin: 0 auto;
